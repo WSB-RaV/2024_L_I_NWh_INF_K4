f59a854759f43f027c9452d0178c586093ca0f46
Rafal Pelc <barca@onet.eu>
2024-04-30 21:57:32 +0200

    linter errors corrected, # noqa was used @ last line in __init__.py

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -23,9 +23,9 @@ def get_formatted(msg, imie, format):
 def format_to_json(msg, imie):
     return '{ "imie": "' + imie + '", "msg": "' + msg + '"}'
 
-#def format_to_json(msg, imie):
-#    return ('{ "imie":"' + imie + '", "msg":' +
-#            msg + '"}')
+# def format_to_json(msg, imie):
+# return ('{ "imie":"' + imie + '", "msg":' +
+# msg + '"}')
 
 
 def plain_text(msg, imie):
